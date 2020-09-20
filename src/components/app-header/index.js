a5f9c55c48c7abe820bd20e89eb90daee0981c5a
wanguan <1326075334@qq.com>
2020-09-20 22:52:05 +0800

    update

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,12 +1,17 @@
 import React, { memo } from 'react'
 import { NavLink } from 'react-router-dom'
+import { HeaderLeft, HeaderRight, HeaderWrapper } from './style'
 
 export default memo(function JMAppHeader() {
   return (
-    <div>
-      <NavLink to="/">发现音乐</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-      <NavLink to="/friend">我的朋友</NavLink>
-    </div>
+    <HeaderWrapper>
+      <div className="content w1100">
+        <HeaderLeft>
+          <h1><a href="#/" className="logo sprite_01">网易云音乐</a></h1>
+        </HeaderLeft>
+        <HeaderRight>right</HeaderRight>
+      </div>
+      <div className="red-line"></div>
+    </HeaderWrapper>
   )
 })
---
+++ src/components/app-header/style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #242221;
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+  }
+
+  .red-line {
+    height: 5px;
+    background-color: #c20b0b;
+  }
+`
+
+export const HeaderLeft = styled.div`
+  .logo {
+    display: block;
+    width: 176px;
+    height: 69px;
+    background-position: 0 0;
+    text-indent: -9999px;
+  }
+`
+export const HeaderRight = styled.div``
