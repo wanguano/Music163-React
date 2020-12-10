508cc4a045ef0602483d676c2bd045ab9f65992d
wanguan <1326075334@qq.com>
2020-12-10 22:12:32 +0800

    初始化歌曲详情

--- src/pages/song-detail/index.js
+++ src/pages/song-detail/index.js
@@ -1,10 +1,12 @@
 import React, { memo } from 'react'
+import { SonglistWrapper } from './style'
 
 export default memo(function JMSonglist() {
   return (
-    <div>
+    <SonglistWrapper>
+      
       <h2>SongDetail
       歌曲详情页</h2>
-    </div>
+    </SonglistWrapper>
   )
 })
---
+++ src/pages/song-detail/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SonglistWrapper = styled.div`
+
+`
+
+export const SonglistContent = styled.div`
+
+`
