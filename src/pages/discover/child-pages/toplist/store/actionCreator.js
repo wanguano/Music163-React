2acdf0b7600c42229cb6af92863e50f7dc649190
wanguan <1326075334@qq.com>
2020-12-26 13:50:38 +0800

    格式化代码

--- src/pages/discover/child-pages/toplist/store/actionCreator.js
+++ src/pages/discover/child-pages/toplist/store/actionCreator.js
@@ -1,64 +1,80 @@
 import * as actionTypes from './actionTypes'
-import {getToplistInfo, getToplistDetail} from '@/service/toplist'
+import { getToplistInfo, getToplistDetail } from '@/service/toplist'
 
 // 改变榜单数据Action
 const changeToplistAction = (toplistInfo) => ({
   type: actionTypes.CHANGE_TOPLIST_COUNT,
-  toplistInfo
+  toplistInfo,
 })
 
 // 改变当前索引Action
 export const changeCurrentIndexAction = (index) => ({
   type: actionTypes.CHANGE_CURRENT_INDEX,
-  index 
+  index,
 })
 
 // 改变当前歌单的ID_Action
 export const changeCurrentToplistIdAction = (id) => ({
   type: actionTypes.CHANGE_CURRENT_TOPLIST_ID,
-  id
+  id,
 })
 
 // 改变榜单标题详情Action
 const changeToplistTitleInfo = (titleInfo) => ({
   type: actionTypes.CHANGE_CURRENT_TOPLIST_TITLE_INFO,
-  titleInfo
+  titleInfo,
 })
 
 // 改变不同榜单列表Action
 const changeCurrentToplist = (toplist) => ({
   type: actionTypes.CHANGE_CURRENT_TOPLIST,
-  toplist 
+  toplist,
 })
 
 // 榜单network
 export const getToplistInfoAction = () => {
-  return dispatch => {
-    getToplistInfo().then(res => {
+  return (dispatch) => {
+    getToplistInfo().then((res) => {
       dispatch(changeToplistAction(res.list))
     })
-  } 
+  }
 }
 
 // 榜单标题信息 network
 export const getToplistTitleInfoAction = (id) => {
-  return dispatch => {
+  return (dispatch) => {
     getToplistDetail(id).then((res) => {
       // 取出榜单标题详情信息
-      const {coverImgUrl,name,trackNumberUpdateTime,playCount,subscribedCount,commentCount,shareCount} = res && res.playlist
-      const toplistTitleInfo = {coverImgUrl,name,trackNumberUpdateTime,playCount,subscribedCount,commentCount,shareCount}
+      const {
+        coverImgUrl,
+        name,
+        trackNumberUpdateTime,
+        playCount,
+        subscribedCount,
+        commentCount,
+        shareCount,
+      } = res && res.playlist
+      const toplistTitleInfo = {
+        coverImgUrl,
+        name,
+        trackNumberUpdateTime,
+        playCount,
+        subscribedCount,
+        commentCount,
+        shareCount,
+      }
       dispatch(changeToplistTitleInfo(toplistTitleInfo))
-    });
+    })
   }
 }
 
 // 榜单列表详情信息 network
 export const getToplistItemAction = (id) => {
-  return dispatch => {
+  return (dispatch) => {
     getToplistDetail(id).then((res) => {
-      // 榜单列表详情信息
+        // 榜单列表详情信息
       const currentToplist = res && res.playlist.tracks
-      dispatch(changeCurrentToplist(currentToplist))  
-    });
+      dispatch(changeCurrentToplist(currentToplist))
+    })
   }
 }
