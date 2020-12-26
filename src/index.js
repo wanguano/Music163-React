80965cb0467484fe66022b765b8285ce564ffaaa
wanguan <1326075334@qq.com>
2020-12-26 18:23:08 +0800

    增加组件公共样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@/assets/css/reset.css'
+import '@/assets/css/component.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
