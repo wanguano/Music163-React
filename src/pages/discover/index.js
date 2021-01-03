9df214aa0634bbedec34bf55cabc9ad95463a2b1
wanguan <1326075334@qq.com>
2021-01-03 18:12:31 +0800

    修改导入

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,9 +1,8 @@
 import React, { memo } from 'react'
-import { CategoryList, HeaderCategory } from './style'
+import { HeaderCategory } from './style'
 
-import { dicoverMenu } from '@/common/local-data'
+import AppNavBar from '@/components/nav-bar'
 
-import { NavLink } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import { useChangeDropBoxState , useGlobalKeyboardEvent} from '@/hooks/change-state'
 
@@ -14,22 +13,10 @@ export default memo(function JMDiscover(props) {
   // custom hook
   useGlobalKeyboardEvent()
 
-  // 通过redux-thunk发送网络请求
   return (
     <HeaderCategory onClick={useChangeDropBoxState()}>
-      <div className="top">
-        <CategoryList className="w1100">
-          {dicoverMenu.map((item) => {
-            return (
-              <li key={item.title} className="item">
-                <NavLink to={item.link} activeClassName="menu-active">
-                  {item.title}
-                </NavLink>
-              </li>
-            )
-          })}
-        </CategoryList>
-      </div>
+      {/* 导航栏 */}
+      <AppNavBar />
       {renderRoutes(route.routes)}
     </HeaderCategory>
   )
