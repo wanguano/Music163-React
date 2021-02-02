9ccaade82d0f82829f6606a99eb82f7380b753e7
wanguan <1326075334@qq.com>
2021-02-02 17:51:37 +0800

    登录接口

---
+++ src/service/login.js
@@ -0,0 +1,27 @@
+import request from './request'
+/* 手机号登录 */
+export function gotoPhoneLogin(phone, password, countrycode, md5_password) {
+  return request({
+    url: '/login/cellphone',
+    method: 'get',
+    params: {
+      phone,
+      password,
+      countrycode,
+      md5_password
+    }
+  })
+}
+
+/* 邮箱登录 */
+export function gotoEmailLogin(email, password, md5_password) {
+  return request({
+    url: '/login',
+    method: 'get',
+    params: {
+      email, 
+      password,
+      md5_password
+    }
+  })
+}
