194a0f027afe3aa2e06e67bcfa57bdddc8964fd2
wanguan <1326075334@qq.com>
2021-02-13 18:14:06 +0800

    添加骨架屏

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { memo, Suspense, useEffect } from 'react'
-import { Provider } from 'react-redux'
+import React, { memo, Suspense, useEffect } from 'react';
+import { Provider } from 'react-redux';
 
-import { renderRoutes } from 'react-router-config'
-import { HashRouter } from 'react-router-dom'
-import routes from './router'
-import store from './store'
+import { renderRoutes } from 'react-router-config';
+import { HashRouter } from 'react-router-dom';
+import routes from './router';
+import store from './store';
 
-import { BackTop, Skeleton } from 'antd'
-import JMAppHeader from 'components/app-header'
-import JMAppFooter from 'components/app-footer'
-import JMAppPlayerBar from './pages/player/app-player-bar'
+import { BackTop, Skeleton } from 'antd';
+import JMAppHeader from 'components/app-header';
+import JMAppFooter from 'components/app-footer';
+import JMAppPlayerBar from './pages/player/app-player-bar';
 export default memo(function App() {
   /*
    *  ?. :保证这个对象前面必须有这个属性才可以调用
@@ -35,30 +35,32 @@ export default memo(function App() {
   // a.b ??= 20
   // // a.c有值吗? 有值就用自己的,没有就用右侧的value
   // a.c ??= 20
+  
+  // other handle
+  const loginState = JSON.parse(localStorage.getItem('isLogin')) ?? false
+  console.log(loginState)
 
   // other hook
   useEffect(() => {
-    /* 
-      * 本地存储操作
-      *   1.保存当前登录状态
-      *   2.标识当前登录用户
-    */
-    // const loginState = JSON.parse(localStorage.getItem('isLogin'))
-
-    
-
-  }, [])
+    /*
+     * 本地存储操作
+     *   1.保存当前登录状态
+     *   2.标识当前登录用户
+     */
+  }, []);
 
   return (
     <Provider store={store}>
       <HashRouter>
         <JMAppHeader />
-        <Suspense fallback={<Skeleton active />}>{renderRoutes(routes)}</Suspense>
+        <Suspense fallback={<Skeleton active />}>
+          {renderRoutes(routes)}
+        </Suspense>
         <JMAppFooter />
         <JMAppPlayerBar />
         {/* 返回顶部 */}
         <BackTop />
       </HashRouter>
     </Provider>
-  )
-})
+  );
+});
