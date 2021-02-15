3f4acfe5646d222b76ece59bcc0dddee27dbf204
wanguan <1326075334@qq.com>
2021-02-15 21:39:06 +0800

    修改action

--- src/components/theme-login/store/reducer.js
+++ src/components/theme-login/store/reducer.js
@@ -2,15 +2,18 @@ import {Map} from 'immutable'
 import * as actionTypes from './actionTypes'
 const defaultState = Map({
   isVisible: false,
-  isLogin: false
+  // isLogin: false,// 登录状态
+  profile: ''
 })
 
 function reducer(state = defaultState, action) {
   switch(action.type) {
     case actionTypes.CHANGE_IS_VISIBLE_STATE:
       return state.set('isVisible', action.isVisible)
-    case actionTypes.CHANGE_IS_LOGIN_STATE:
-      return state.set('isLogin', action.isLogin)
+    // case actionTypes.CHANGE_IS_LOGIN_STATE:
+    //   return state.set('isLogin', action.isLogin)
+    case actionTypes.CHANGE_PROFILE_INFO:
+      return state.set('profile', action.profile)
     default: 
       return state
   }
