e4d3afc23cb1368c1bd6773d2bf6ebb6fccd5c22
wanguan <1326075334@qq.com>
2021-02-17 21:50:19 +0800

    全局搜索框快捷键

--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -2,11 +2,15 @@ import React, { memo } from 'react'
 import { SongDetailWrapper, SongLeft, SongRight } from './style'
 import SongInfo from './child-pages/song-info'
 import SongComment from './child-pages/song-comment'
+import { useGlobalKeyboardEvent } from '../../hooks/change-state'
 
 // 歌曲详情页面
 export default memo(function JMSongDetail(props) {
   // props/state  先写死: 167876    到时候换这个: props.location.state.id
   // 之后根路id发送请求,数据保存在redux当中
+
+  // custom hook
+  useGlobalKeyboardEvent()// 全局注册 ctrl+k 唤醒下拉框
   return (
     <SongDetailWrapper>
       <div className="content w980">
