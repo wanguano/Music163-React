493a1b2a0d2a33dcf0193cdf9bb86aa0a2a98d65
wanguan <1326075334@qq.com>
2021-02-17 21:49:16 +0800

    404返回主页

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,6 +1,6 @@
-import { Result, Button } from 'antd';
-
 import React, { memo } from 'react'
+import { Result, Button } from 'antd';
+import {RollbackOutlined} from '@ant-design/icons'
 
 export default memo(function ErrorPage(props) {
   // props/state
@@ -15,7 +15,7 @@ export default memo(function ErrorPage(props) {
       status="404"
       title="404"
       subTitle="对不起，您访问的页面不存在。"
-      extra={<Button type="primary" onClick={handleGoHomeBack}>返回首页</Button>}
+      extra={<Button type="primary" icon={<RollbackOutlined />} onClick={handleGoHomeBack}>返回首页</Button>}
     />
   )
 })
