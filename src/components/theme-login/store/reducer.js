0fd0e249fd85d247cb3aa4469f37c0e4bcdace04
wanguan <1326075334@qq.com>
2021-02-20 13:54:55 +0800

    登录状态

--- src/components/theme-login/store/reducer.js
+++ src/components/theme-login/store/reducer.js
@@ -2,7 +2,7 @@ import {Map} from 'immutable'
 import * as actionTypes from './actionTypes'
 const defaultState = Map({
   isVisible: false,
-  // isLogin: false,// 登录状态
+  isLogin: false,// 登录状态
   profile: ''
 })
 
@@ -10,8 +10,8 @@ function reducer(state = defaultState, action) {
   switch(action.type) {
     case actionTypes.CHANGE_IS_VISIBLE_STATE:
       return state.set('isVisible', action.isVisible)
-    // case actionTypes.CHANGE_IS_LOGIN_STATE:
-    //   return state.set('isLogin', action.isLogin)
+    case actionTypes.CHANGE_USER_LOGIN_STATE:
+      return state.set('isLogin', action.isLogin)
     case actionTypes.CHANGE_PROFILE_INFO:
       return state.set('profile', action.profile)
     default: 
