2be4493333325a3b9c6d34e99b061c153007b541
wanguan <1326075334@qq.com>
2021-02-20 13:54:44 +0800

    登录Action

--- src/components/theme-login/store/actionCreator.js
+++ src/components/theme-login/store/actionCreator.js
@@ -1,5 +1,8 @@
 import { gotoPhoneLogin } from '@/service/login'
+import { message } from 'antd'
 import * as actionTypes from './actionTypes'
+import loginInfo from '@/config/token'
+import { getLoginInfo , setLoginInfo} from '@/utils/secret-key'
 // 更改登录框显示
 export const changeIsVisible = (visibleState) => ({
   type: actionTypes.CHANGE_IS_VISIBLE_STATE,
@@ -12,22 +15,35 @@ export const changeUserProfile = (profileInfo) => ({
   profile: profileInfo
 })
 
+// 更改登录状态
+export const changeUserLoginState = (loginState) => ({
+  type: actionTypes.CHANGE_USER_LOGIN_STATE,
+  isLogin: loginState
+})
+
 
 // -------------获取登录信息-------------
-export const getLoginProfileInfo = (username, password, message) => {
+export const getLoginProfileInfo = (username, password, tip) => {
   return (dispatch) => {
     gotoPhoneLogin(username, password).then((res) => {
       if (res.code !== 200) {
         message.error('账号或密码错误')
       }else {
-        message.success('登录成功')
+        tip && message.success('登录成功')
         console.log(res)
         // 登录成功
         document.cookie = res.cookie
         // 保存登录信息
         dispatch(changeUserProfile(res && res.profile))
         // 更改登录状态
-        // localStorage.setItem('isLogin', true)
+        dispatch(changeUserLoginState(true))
+        // 更改登录状态
+        loginInfo.username = username
+        loginInfo.password = password
+        loginInfo.state = true
+        let newLoginInfo = Object.assign(getLoginInfo('loginInfo'), loginInfo)
+        setLoginInfo('loginInfo', newLoginInfo)
+        console.log(getLoginInfo('loginInfo'))
       }
     })
   }
