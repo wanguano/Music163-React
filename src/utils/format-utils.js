981ceabcd9f6ab7161b832403cc1847ca0b63c95
吴建明 <1326075334@qq.com>
2021-02-26 15:18:12 +0800

    添加工具类: 获取查询字符串

--- src/utils/format-utils.js
+++ src/utils/format-utils.js
@@ -107,3 +107,22 @@ export function getMatchReg(loginState){
       return '';
   }
 }
+
+/**
+ * 获取url参数
+ * @param name 参数名
+ */
+export function getQueryObject(url) {
+  url = url == null ? window.location.href : url
+  const search = url.substring(url.lastIndexOf('?') + 1)
+  const obj = {}
+  const reg = /([^?&=]+)=([^?&=]*)/g
+  search.replace(reg, (rs, $1, $2) => {
+    const name = decodeURIComponent($1)
+    let val = decodeURIComponent($2)
+    val = String(val)
+    obj[name] = val
+    return rs
+  })
+  return obj
+}
