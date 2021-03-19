bec28578783855ade08b125f5451860ae8ed0390
吴建明 <1326075334@qq.com>
2021-03-19 10:33:18 +0800

    APP根组件添加包裹层

---
+++ src/components/theme-dialog/index.js
@@ -0,0 +1,35 @@
+import { Modal, Button } from 'antd';
+import React, { memo, useState } from 'react';
+import propTypes from 'prop-types'
+
+function ThemeDialog(props) {
+  // props/state
+  const {controlShow, title, handleOk, handleCancel} = props
+
+  return (
+    <>
+      <Modal
+        title={title}
+        visible={controlShow}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        {props.children}
+      </Modal>
+    </>
+  );
+};
+
+// 校验传递的props
+ThemeDialog.propTypes = {
+  controlShow: propTypes.bool.isRequired,
+  title: propTypes.string
+}
+
+// 组件默认的props
+ThemeDialog.defaultProps = {
+  controlShow: false,
+  title: 'hello dialog'
+}
+
+export default memo(ThemeDialog)
---
+++ src/pages/app/index.js
@@ -0,0 +1,55 @@
+import React, { memo, Suspense, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
+import routes from '@/router';
+import { Skeleton, Button } from 'antd';
+import ThemeDialog from '@/components/theme-dialog/index'
+import initLoginInfo from '@/config/token.js';
+import { setLoginInfo, getLoginInfo } from '@/utils/secret-key';
+import { getLoginProfileInfo } from '@/components/theme-login/store/actionCreator';
+
+export default memo(function APPWrapper() {
+  // props/state
+  const [isShow, setIsShow] = useState(false)
+
+  // redux hook
+  const dispatch = useDispatch();
+  console.log('初始化登录~~~');
+
+  // other handle
+  // 初始化
+  const initLogin = () => {
+    // 存在登录信息
+    if (localStorage.getItem('loginInfo') != null) {
+      const { username, password } = getLoginInfo('loginInfo');
+      username && password
+        ? dispatch(getLoginProfileInfo(username, password))
+        : console.log('当前登录的默认信息');
+    }
+    // 不存在登录信息
+    else {
+      setLoginInfo('loginInfo', initLoginInfo);
+    }
+  };
+  initLogin();
+
+  // other function
+  const handleOk = () => {
+    setIsShow(false);
+  };
+
+  const handleCancel = () => {
+    setIsShow(false);
+  };
+
+  return (
+    <>
+      <Suspense fallback={<Skeleton active />}>{renderRoutes(routes)}</Suspense>
+      <ThemeDialog controlShow={isShow} title="上传音乐" handleOk={handleOk} handleCancel={handleCancel}>
+        <h2>hello dialog</h2>
+        <h3>上传音乐</h3>
+      </ThemeDialog>
+      <Button onClick={() => setIsShow(!isShow)}>点我</Button>
+    </>
+  );
+});
