82fa2ddd0435dd871f3a2c4abfa80cccd9940531
吴建明 <1326075334@qq.com>
2021-03-24 15:52:34 +0800

    公网IP更改

--- src/service/config.js
+++ src/service/config.js
@@ -1,9 +1,11 @@
 // 本地测试API 
 // const devBaseURL = "http://localhost:3000";
-// const proBaseURL = "http://localhost:3000";
+// const proBaseURL = "http://localho st:3000";
 // 已经部署到服务器上的API
-const devBaseURL = "http://123.57.176.198:3000";
-const proBaseURL = "http://123.57.176.198:3000";
+const devBaseURL = "http://39.102.36.212:3000";
+const proBaseURL = "http://39.102.36.212:3000";
+// const devBaseURL = "http://123.207.32.32:9001/";
+// const proBaseURL = "http://123.207.32.32:9001/";
 export const BASE_URL =
   process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL
 
