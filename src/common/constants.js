2dee3b93511c696d4e2a467f44c6ac8ae3a12cb3
吴建明 <1326075334@qq.com>
2021-04-27 16:10:39 +0800

    本次存储抽离常量

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,5 +2,8 @@ export const HOT_RECOMMEND_LIMIT = 8;
 export const SETTLE_SINGER_COUNT = 5;
 
 export const SONG_LIST_LIMIT = 35
-
-export const SONG_PLAYLIST_ID = [167876, 411214279, 362998, 1363948882, 1387581250].reverse()
+// 默认音乐id
+export const SONG_PLAYLIST_ID = [167876, 411214279, 1483891561, 1363948882, 1387581250]
+// 本地存储音乐Key
+export const LOCAL_PLAYLIST_ID_KEY = 'playlistId' // 歌曲列表id
+export const LOCAL_CURRENT_SONG_INDEX_KEY = 'currentSongIndex' // 歌曲所引
